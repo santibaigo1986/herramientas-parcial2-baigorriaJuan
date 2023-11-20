c3fb620d08a975c77bdbb8028be71bd58488bf65
Baigorria Juan <santibaigo1986@gmail.com>
2023-11-20 12:01:09 -0300

    Lista Actores

--- BaigorriaJuan/Controllers/ActorsController.cs
+++ BaigorriaJuan/Controllers/ActorsController.cs
@@ -14,7 +14,7 @@ namespace BaigorriaJuan.Controllers
         public IActionResult Index()
         {
             var data = _context.Actors.ToList();
-            return View();
+            return View(data);
         }
     }
 }
