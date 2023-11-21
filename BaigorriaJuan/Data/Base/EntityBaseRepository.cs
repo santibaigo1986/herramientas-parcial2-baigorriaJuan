8d42a3beb9bde5453f4d70b93373fa6d4c580b74
Baigorria Juan <santibaigo1986@gmail.com>
2023-11-21 12:01:17 -0300

    detailsfuncionando40

--- BaigorriaJuan/Data/Base/EntityBaseRepository.cs
+++ BaigorriaJuan/Data/Base/EntityBaseRepository.cs
@@ -1,26 +1,24 @@
-﻿namespace BaigorriaJuan.Data.Base
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BaigorriaJuan.Data.Base
 {
     public class EntityBaseRepository<T> : IEntityBaseRepository<T> where T : class, IEntityBase, new()
     {
-        public Task AddAsync(T entity)
+        private readonly AppDbContext _context;
+        public EntityBaseRepository(AppDbContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
         }
+
+        public async Task AddAsync(T entity) => await _context.Set<T>().AddAsync(entity);
 
         public Task DeleteAsync(int id)
         {
             throw new NotImplementedException();
         }
 
-        public Task<IEnumerable<T>> GetAllAsync()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<T> GetByIdAsync(int id)
-        {
-            throw new NotImplementedException();
-        }
+        public async Task<IEnumerable<T>> GetAllAsync() => await _context.Set<T>().ToListAsync();
+        public async Task<T> GetByIdAsync(int id) => await _context.Set<T>().FirstOrDefaultAsync(n => n.Id == id);
 
         public Task<T> UpdateAsync(int id, T entity)
         {
