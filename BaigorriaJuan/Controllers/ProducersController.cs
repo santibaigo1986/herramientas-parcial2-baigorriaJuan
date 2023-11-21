16fc91b25fda158728d8dcdfa627d12d07525ea4
Baigorria Juan <santibaigo1986@gmail.com>
2023-11-21 14:19:37 -0300

    sofar46

--- BaigorriaJuan/Controllers/ProducersController.cs
+++ BaigorriaJuan/Controllers/ProducersController.cs
@@ -1,5 +1,6 @@
 ﻿using BaigorriaJuan.Data;
 using BaigorriaJuan.Data.Services;
+using BaigorriaJuan.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -18,6 +19,47 @@ namespace BaigorriaJuan.Controllers
         {
             var allProducers = await _service.GetAllAsync();
             return View(allProducers);
+        }
+        //GET: productores/detalles/1
+        public async Task<IActionResult> Details(int id)
+        {
+            var producerDetails = await _service.GetByIdAsync(id);
+            if (producerDetails == null) return View("NotFound");
+            return View(producerDetails);
+        }
+        //GET: producers/crear
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("ProfilePictureURL,FullName,Bio")] Producer producer)
+        {
+            if (!ModelState.IsValid) return View(producer);
+
+            await _service.AddAsync(producer);
+            return RedirectToAction(nameof(Index));
+        }
+        //GET: producers/edit/1
+        public async Task<IActionResult> Edit(int id)
+        {
+            var producerDetails = await _service.GetByIdAsync(id);
+            if (producerDetails == null) return View("NotFound");
+            return View(producerDetails);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,ProfilePictureURL,FullName,Bio")] Producer producer)
+        {
+            if (!ModelState.IsValid) return View(producer);
+
+            if (id == producer.Id)
+            {
+                await _service.UpdateAsync(id, producer);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(producer);
         }
     }
 }
