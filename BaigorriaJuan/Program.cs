0b62e36d608c330792b437f01ac8af652b3da2ed
Manuel Fondovila <mfondovila@riu.com>
2023-11-28 20:07:33 -0300

    add movie detail

--- BaigorriaJuan/Program.cs
+++ BaigorriaJuan/Program.cs
@@ -35,7 +35,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Movies}/{action=Index}/{id?}");
 
 app.Run();
 
